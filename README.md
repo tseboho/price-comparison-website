2a95d5544e61e5111d9ef629f11d43810aff3cbb
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-01-01 10:24:30 +0200

    feat(README.md-src/obj.py): added validation on price field

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 A manual search on the command line:
 ```bash
 uv run python -m src.dev.cli_product_search woolworths 'romany creams' 5
+uv run python -m src.dev.cli_product_search picknpay 'romany creams' 5
 ```
--- src/obj.py
+++ src/obj.py
@@ -1,9 +1,20 @@
-from pydantic import BaseModel, ConfigDict
+from typing import Annotated
+
+import pydantic
 
 
-class ItemSearchResult(BaseModel):
-    model_config = ConfigDict(frozen=True)
+def is_non_negative(value: int) -> int:
+    """Validates that input integer is non-negative.
+    Used for pydantic field validation
+    """
+    if value < 0:
+        raise ValueError(f"{value:,} is negative")
+    return value
+
+
+class ItemSearchResult(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(frozen=True)
 
     item_title: str
     item_image_url: str
-    price_cents: int
+    price_cents: Annotated[int, pydantic.AfterValidator(is_non_negative)]
